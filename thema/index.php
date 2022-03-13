60e3ba154e918e99e33dbd30db2cef4cc03c67d8
Pedro da Silveira Azeredo <pedro.azeredo93@gmail.com>
2022-03-13 02:06:53 -0300

    feat: criando form

--- thema/index.php
+++ thema/index.php
@@ -1,3 +1,4 @@
 <?php include("header.php"); ?>
+<?php include("partials/form.php"); ?>
 <?php include("partials/mapa.php"); ?>
 <?php include("footer.php"); ?>
---
+++ thema/partials/form.php
@@ -0,0 +1,63 @@
+<section class="pt-5">
+    <div class="container">
+        <div class="row justify-content-center align-items-end">
+            <div class="col-lg-6 text-end pe-lg-0">
+                <img src="assets/img/img-form.png" alt="">
+            </div>
+            <div class="col-lg-6 ps-lg-0">
+                <div class="row my-3 justify-content-center align-items-center">
+                    <div class="col">
+                        <h2 class="text-primary">Fale conosco</h2>
+                    </div>
+                </div>
+                <form action="">
+
+                    <div class="form-group row py-2">
+                        <div class="col field">
+                            <input type="text" class="form-control py-3" id="nome" name="nome" required>
+                            <label for="nome" class="px-2">Nome</label>
+                        </div>
+                        <div class="col field">
+                            <input type="email" class="form-control py-3" id="email" name="email" required>
+                            <label for="email" class="px-2">E-mail</label>
+                        </div>
+                    </div>
+
+
+                    <div class="form-group row py-2">
+                        <div class="col field">
+                            <input type="tel" class="form-control py-3" id="telefone" name="telefone" required>
+                            <label for="telefone" class="px-2">Telefone/WhatsApp</label>
+                        </div>
+                        <div class="col field">
+                            <input type="text" class="form-control py-3" id="assunto" name="assunto" required>
+                            <label for="assunto" class="px-2">Assunto</label>
+                        </div>
+                    </div>
+
+                    <div class="form-group row py-2">
+                        <div class="col field">
+                            <textarea name="" id="" cols="30" rows="5" class="form-control" placeholder="Mensagem"></textarea>
+                        </div>
+                    </div>
+
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+                        <label class="form-check-label" for="inlineCheckbox1">Aceito receber e-mails.</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+                        <label class="form-check-label" for="inlineCheckbox2">Li e aceito os termos de Política de Privacidade.</label>
+                    </div>
+                    <div class="form-group row my-2">
+                        <div class="col-lg-3 my-2">
+                            <div class="d-grid">
+                                <button type="submit" class="btn btn-primary">Enviar</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
